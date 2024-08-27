39e2f81be104ee6e4389998ea5d8800e564d4bbb
T Mohamed Yaser <140265777+Yaser-123@users.noreply.github.com>
2024-08-27 21:26:10 +0530

    Add files via upload

---
+++ Fastest-Text-to-Image-Generator-main/README.md
@@ -0,0 +1,45 @@
+# Fastest-Text-to-Image-Generator
+Fastest Text-to-Image Generator using fal ai.
+
+
+
+## Prerequisites:  
+[Fal AI](https://fal.ai/) API key (for model access)  
+
+
+
+## Getting Started :white_check_mark:  
+To get started with this project, you'll need to clone the repository and have [Node]((https://nodejs.org/en/download) installed on your system.  
+
+  
+### Cloning the Repository :inbox_tray:
+Run the following command to clone the repository:  
+
+```
+git clone https://github.com/ChatTeach/Fastest-Text-to-Image-Generator.git
+```
+
+### Install Dependencies :wrench: 
+Navigate to the project directory:
+```
+cd Fastest-Text-to-Image-Generator
+```
+
+Install the dependencies:
+```
+npm install -i
+```
+## Running the Application :rocket:
+To run the application, run the following command:
+```
+npm run dev
+```
+
+Access the application in your browser using the URL:
+```
+http://127.0.0.1:3000
+```
+or
+```
+http://localhost:3000
+```
---
+++ Fastest-Text-to-Image-Generator-main/components/nav.tsx
@@ -0,0 +1,44 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import { ModelIcon } from "@/components/icons/model-icon";
+import Link from "next/link";
+import { Space_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { GithubIcon } from "lucide-react";
+
+const spaceMono = Space_Mono({
+  weight: "400",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export function Nav() {
+  return (
+    <div className="h-14 py-2 px-2 md:px-8 border-b flex items-center">
+      <div className="flex flex-1 items-center">
+        <Link href="/">
+          <h1 className={cn("font-light text-xl", spaceMono.className)}>
+            <span className="text-[#00f9ff]">Vertex</span>
+            <span>🖌️</span>
+            <span className="text-[#8ffcff]">Art</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="flex flex-none items-center space-x-4">
+        <ThemeToggle />
+        <Button size="sm" asChild>
+          <Link
+            href="https://github.com/Yaser-123/Text-to-image"
+            className="flex flex-row space-x-2 items-center bg-[#0900ff] text-[#8ffcff]"
+            target="_blank"
+          >
+            <GithubIcon className="h-5 w-5" />
+            <span>
+              <span className="hidden md:inline-block">Fork on</span> GitHub
+            </span>
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
